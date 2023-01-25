6a4b3ecfc1da0d99054159ba33f4427458d34d5c
Robert Floyd <rbrt@hey.com>
2023-01-25 18:10:03 -0500

    Update nuxt.config.ts

--- healthcareos-demo/nuxt.config.ts
+++ healthcareos-demo/nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      charset: "utf-16",
+      title: "Demo | HealthcareOS",
+      meta: [
+        // <meta name="description" content="My amazing site">
+        { name: "description", content: "HealthcareOS from Alloy Health" },
+      ],
+    },
+  },
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase", "nuxt-headlessui", "@nathanchase/nuxt-dayjs-module"],
   css: ["@fortawesome/fontawesome-svg-core/styles.css", "@/assets/styles/main.css"],
   headlessui: {
