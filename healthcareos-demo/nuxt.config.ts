60ae1e01ce0c1757375d0735bd4555f96ae03430
Robert Floyd <rbrt@hey.com>
2023-01-25 14:00:30 -0500

    add posthog

--- healthcareos-demo/nuxt.config.ts
+++ healthcareos-demo/nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/supabase", "nuxt-headlessui", "@nathanchase/nuxt-dayjs-module"],
   css: ["@fortawesome/fontawesome-svg-core/styles.css", "@/assets/styles/main.css"],
+  plugins: [{ src: "./plugins/posthog", mode: "client" }],
   headlessui: {
     prefix: "Headless",
   },
---
+++ healthcareos-demo/plugins/posthog/index.js
@@ -0,0 +1,26 @@
+import posthog from "posthog-js";
+//import Vue from "vue";
+
+export default function ({ app: { router } }, inject) {
+  // Init PostHog
+  posthog.init(phc_mbCPc8pLdEzaePrHJkRJS8ciGyhEbPzSbT4nijoILrW, {
+    api_host: "https://app.posthog.com",
+    capture_pageview: true,
+    // loaded: () => posthog.identify("unique_id"), // If you can already identify your user
+  });
+
+  // Inject PostHog into the application and make it available via this.$posthog (or app.$posthog)
+  inject("posthog", posthog);
+
+  // Make sure that pageviews are captured with each route change
+  router.afterEach((to) => {
+    Vue.nextTick(() => {
+      /* Note: this might also be a good place to call posthog.register(...) in order to update your properties
+      on each page view
+      */
+      posthog.capture("$pageview", {
+        $current_url: to.fullPath,
+      });
+    });
+  });
+}
