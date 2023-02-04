01f4eac9ada7a266937dfaac3f0dfe2bf8f16cf5
Robert Floyd <rbrt@hey.com>
2023-02-04 16:43:10 -0500

    table demo experiment

--- healthcareos-demo/pages/appointments/[id].vue
+++ healthcareos-demo/pages/appointments/[id].vue
@@ -1,5 +1,8 @@
 <template>
-  <NuxtLayout :name="'space'" :pages="pages" :items="todayAppointments">
+  <NuxtLayout
+    :name="'space'"
+    :pages="pages"
+    :items="todayAppointments">
     <template #pageContent>
       <div class="flex flex-row flex-grow">
         <patient-overview-module
@@ -20,7 +23,9 @@
           <span>{{ nextItem.patient.name }}</span>
           <font-awesome-icon icon="fa-regular fa-arrow-down" />
         </NuxtLink>
-        <button class="py-5 px-6 border border-slate-200 rounded-xl" v-if="prevItem">
+        <button
+          class="py-5 px-6 border border-slate-200 rounded-xl"
+          v-if="prevItem">
           <span class="font-semibold">Previous</span>
           {{ prevItem.patient.name }}
           <font-awesome-icon icon="fa-regular fa-arrow-up" />
@@ -31,52 +36,60 @@
 </template>
 
 <script setup>
-import PatientOverviewModule from "~/components/Cards/Modules/ModuleTemplates/PatientOverviewModule.vue";
-const route = useRoute();
-const id = route.params.id;
+  import PatientOverviewModule from '~/components/Cards/Modules/ModuleTemplates/PatientOverviewModule.vue';
+  const route = useRoute();
+  const id = route.params.id;
 
-const appointments = useState("appointments");
+  const appointments = useState('appointments');
 
-const thisAppointment = appointments.value.find((appointments) => appointments.encounter_id == id);
+  const thisAppointment = appointments.value.find(
+    (appointments) => appointments.encounter_id == id
+  );
 
-const appointmentDetail = useState("thisAppointment", () => thisAppointment);
+  //const appointmentDetail = useState("thisAppointment", () => thisAppointment);
 
-const todayAppointments = appointments.value.map((appointment) => ({
-  id: appointment.encounter_id,
-  heading: appointment.patient.name,
-  subHeading: appointment.encounter_time,
-  status: appointment.encounter_status,
-}));
+  const todayAppointments = appointments.value.map((appointment) => ({
+    id: appointment.encounter_id,
+    heading: appointment.patient.name,
+    subHeading: appointment.encounter_time,
+    status: appointment.encounter_status,
+  }));
 
-const pages = [
-  {
-    name: "Today",
-    path: "/",
-    index: 0,
-  },
-  {
-    name: "Appointments",
-    path: "/appointments/" + thisAppointment.encounter_id,
-    index: 1,
-  },
-  {
-    name: thisAppointment.patient.name,
-    path: "/appointments/" + thisAppointment.encounter_id,
-    index: 2,
-  },
-];
+  const pages = [
+    {
+      name: 'Today',
+      path: '/',
+      index: 0,
+    },
+    {
+      name: 'Appointments',
+      path: '/appointments/' + thisAppointment.encounter_id,
+      index: 1,
+    },
+    {
+      name: thisAppointment.patient.name,
+      path: '/appointments/' + thisAppointment.encounter_id,
+      index: 2,
+    },
+  ];
 
-const nextItem = computed(() => {
-  const filteredNext = appointments.value.filter((appointment) => {
-    return appointment != thisAppointment.encounter_time && appointment.encounter_time > thisAppointment.encounter_time;
+  const nextItem = computed(() => {
+    const filteredNext = appointments.value.filter((appointment) => {
+      return (
+        appointment != thisAppointment.encounter_time &&
+        appointment.encounter_time > thisAppointment.encounter_time
+      );
+    });
+    return filteredNext.length ? filteredNext[0] : null;
   });
-  return filteredNext.length ? filteredNext[0] : null;
-});
 
-const prevItem = computed(() => {
-  const filteredPrev = appointments.value.filter((appointment) => {
-    return appointment != thisAppointment.encounter_time && appointment.encounter_time < thisAppointment.encounter_time;
+  const prevItem = computed(() => {
+    const filteredPrev = appointments.value.filter((appointment) => {
+      return (
+        appointment != thisAppointment.encounter_time &&
+        appointment.encounter_time < thisAppointment.encounter_time
+      );
+    });
+    return filteredPrev.length ? filteredPrev[0] : null;
   });
-  return filteredPrev.length ? filteredPrev[0] : null;
-});
 </script>
--- healthcareos-demo/pages/table-demo.vue
+++ healthcareos-demo/pages/table-demo.vue
@@ -3,19 +3,37 @@
     <div class="container">
       <EasyDataTable
         :headers="headers"
-        :items="appointments" />
+        :items="tableItems"
+        hide-footer>
+        <template #item-encounter_link="{ encounter_link }">
+          <NuxtLink :to="encounter_link">Visit</NuxtLink>
+        </template>
+      </EasyDataTable>
     </div>
   </NuxtLayout>
 </template>
 
-<script lang="ts" setup>
-  import type { Header, Item } from 'vue3-easy-data-table';
+<script setup>
+  //import type { Header, Item } from 'vue3-easy-data-table';
 
   const appointments = useState('appointments');
 
-  const headers: Header[] = [
+  const headers = [
     { text: 'Status', value: 'encounter_status', sortable: true },
+    { text: 'Link', value: 'encounter_link' },
   ];
+
+  const tableItems = appointments.value.map((appointment) => ({
+    encounter_status: appointment.encounter_status,
+    encounter_link: '/appointments/' + appointment.encounter_id,
+  }));
+
+  //   const items = appointments.map((appointment) => ({
+  //     id: appointment.encounter_id,
+  //     heading: appointment.patient.name,
+  //     subHeading: appointment.encounter_time,
+  //     status: appointment.encounter_status,
+  //   }));
 
   //   const items: Item[] = [
   //     {
